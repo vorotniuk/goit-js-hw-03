1ed516bc11a6b82a6202a9ecc35b3e0a163e174c
Yaroslav Vorotniuk <slavikvorotnuk@gmail.com>
2023-11-26 19:14:18 +0200

    push

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+    return title.toLowerCase().split(' ').join('-');
+}
+
+const title = "This is a Sample Title";
+const slug = slugify(title);
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
